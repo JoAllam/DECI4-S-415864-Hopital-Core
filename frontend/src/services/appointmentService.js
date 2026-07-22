3db0672ac303abed3c6eb08eb63e23133ff69dc1
Jo Allam <joallam2008@gmail.com>
2026-07-22 19:40:40 +0300

    Adding the other vercel fix

--- frontend/src/services/appointmentService.js
+++ frontend/src/services/appointmentService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = "http://localhost:5001/api/appointments";
+const API =
+  process.env.REACT_APP_APPOINTMENT_API_URL ||
+  "/api/appointments";
 
 export const getAppointments = async () => {
   const response = await axios.get(API);
